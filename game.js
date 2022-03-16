198c81b9bb69f3af83a9d0b182d5ac9b94a130e0
Denis Usalev <denis.ultimate@gmail.com>
2022-03-17 00:23:42 +0200

    added ui

--- game.js
+++ game.js
@@ -7,28 +7,53 @@ let computerPlay = function() {
 
 let playRound = function(playerSelection, computerSelection) {
     playerSelectionLowercase = playerSelection.toLowerCase();
-    if (playerSelectionLowercase == computerSelection) {
-        return "It's a draw! You both chose " + computerSelection;
+    if (playerSelectionLowercase == computerSelection.toLowerCase()) {
+        return [0, "It's a draw! You both chose " + computerSelection];
     } else if (playerSelectionLowercase == "rock" && computerSelection == "Paper") {
-        return "You Lose! Paper beats Rock";
+        return [1, "You Lose! Paper beats Rock"];
     } else if (playerSelectionLowercase == "paper" && computerSelection == "Scissors") {
-        return "You Lose! Scissors beat Paper";
+        return [1, "You Lose! Scissors beat Paper"];
     } else if (playerSelectionLowercase == "scissors" && computerSelection == "Rock") {
-        return "You Lose! Rock beats Scissors";
+        return [1, "You Lose! Rock beats Scissors"];
     } else if (playerSelectionLowercase == "rock" && computerSelection == "Scissors") {
-        return "You Win! Rock beats Scissors";
+        return [2, "You Win! Rock beats Scissors"];
     } else if (playerSelectionLowercase == "paper" && computerSelection == "Rock") {
-        return "You Win! Paper beats Rock";
+        return [2, "You Win! Paper beats Rock"];
     } else {
-        return "You Win! Scissors beat Paper";
+        return [2, "You Win! Scissors beat Paper"];
     }
 }
 
-let game = function() {
-    for (let i = 0; i < 5; i++) {
-        let userSelection = prompt("Rock, Paper or Scissors? Type your selection: ");
-        console.log(playRound(userSelection, computerPlay()));
-    }
-}
+let playerScore = 0;
+let computerScore = 0;
 
-game();
+const buttons = document.querySelectorAll("button");
+buttons.forEach(button => {
+    button.addEventListener('mousedown', (e) => {
+        e.target.classList.add('clicked');
+    })
+    button.addEventListener('mouseup', (e) => {
+        e.target.classList.remove('clicked');
+    })
+    button.addEventListener('click', (e) => {
+        if (playerScore < 5 && computerScore < 5) {
+            let [outcomeCode, outcomeText] = playRound(e.target.textContent, computerPlay());
+            result = document.querySelector('#result')
+            result.textContent = outcomeText;
+            switch (outcomeCode) {
+                case 0:
+                    result.style.color = "black";
+                    break;
+                case 1:
+                    result.style.color = "red";
+                    computerScore += 1;
+                    break;
+                case 2:
+                    result.style.color = "green";
+                    playerScore += 1;
+                    break;
+            }
+            document.querySelector('#scores').textContent = `Computer scored: ${computerScore}. You scored: ${playerScore}.`;
+        };
+    })
+});
